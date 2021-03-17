c4671589c739117feb308fb18af8814df49c5bb0
SelDanilEv <47692381+SelDanilEv@users.noreply.github.com>
2021-03-17 23:25:06 +0300

    Add 404 handle

---
+++ Lab4/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab4.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            ViewBag.url = Request.Url.AbsoluteUri.Split(';').Last();
+            ViewBag.method = Request.HttpMethod;
+            return View();
+        }
+
+        public ActionResult Internal()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+    }
+}
